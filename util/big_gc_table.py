875cce2336f47392248769aec14153078e370634
Yun-Fang Juan <yunfangjuan@gmail.com>
2017-04-26 23:20:47 +0000

    update big_gc script to generate more readable metadata

--- util/big_gc_table.py
+++ util/big_gc_table.py
@@ -19,6 +19,18 @@ import subprocess
 
 S3_BUCKET = 's3://czi-hca/data'
 SRR_GSM_MAPPING = {} # TODO(yf): make this less hacky
+MUS_NCBI_TAXON_ID = '10090'
+HOMO_NCBI_TAXON_ID = '9606'
+METADATA_TOP_SAMPLE_FIELDS = ['geo_accession', 'title', 'treatment_protocol_ch1',
+                     'taxid_ch1', 'characteristics_ch1', 'platform_id']
+METADATA_TOP_SERIES_FIELDS = ['title', 'overall_design', 'summary', 'platform_id',
+                     'pubmed_id', 'sample_taxid']
+
+def taxon_name_to_id(taxon_name):
+    if taxon_name == 'mus':
+        return MUS_NCBI_TAXON_ID
+    if taxon_name == 'homo':
+        return HOMO_NCBI_TAXON_ID
 
 def generate_metadata_objects(gse_file):
     gse = GEOparse.get_GEO(filepath=gse_file)
@@ -38,6 +50,33 @@ def generate_metadata_objects(gse_file):
                         'platform_data':platform_data}
     return samples
 
+def extract_top_metadata_sample_fields(samples_obj, taxon_id, doc_id):
+    output = []
+    sample_list =[]
+    for (key, sample) in samples_obj.iteritems():
+        taxons = sample['sample_data']['taxid_ch1']
+        if taxon_id not in taxons: # taxon doesn't match
+            continue
+        sample_list.append(key)
+        row = [doc_id]
+        for f in METADATA_TOP_SAMPLE_FIELDS:
+            val = sample['sample_data'].get(f, '')
+            if isinstance(val, list):
+                val = ";".join(val)
+            row.append(val)
+        output.append(row)
+    return (sample_list, output)
+
+def extract_top_metadata_series_fields(samples_obj, doc_id):
+    output = [doc_id]
+    series_data = samples_obj.values()[0]['series_data']
+    for f in METADATA_TOP_SERIES_FIELDS:
+        val = series_data.get(f, '')
+        if isinstance(val, list):
+            val = ";".join(val)
+        output.append(val)
+    return output
+
 def gen_metadata_file(doc_id, output_dir):
     try:
         s3_source = S3_BUCKET + '/' + doc_id + '/metadata/'
@@ -54,6 +93,7 @@ def gen_metadata_file(doc_id, output_dir):
             output_file = "%s/%s.metadata.json" % (output_dir, doc_id)
             with open(output_file, 'wb') as csvfile:
                 csvfile.write(json.dumps(samples_obj))
+            return samples_obj
     except Exception:
         return
 
@@ -139,8 +179,9 @@ def get_gene_to_idx_mapping(ht_seq_path):
     return gene_to_idx
 
 
-def generate_gc_table(htseq_list, srr_to_gsm_map, gene_to_idx_table):
+def generate_gc_table(htseq_list, gene_to_idx_table, doc_sample_list):
     cells = []
+    doc_to_counts = {}
     for htseq_file in htseq_list:
         cell_data = [0] * len(gene_to_idx_table)
         with open(htseq_file, "r") as ins:
@@ -157,10 +198,17 @@ def generate_gc_table(htseq_list, srr_to_gsm_map, gene_to_idx_table):
             cell_info['srr_id'] = m.group(2)
             cell_info['taxon'] = m.group(3)
             cell_info['gsm_id'] = SRR_GSM_MAPPING.get(m.group(2)) or ''
-        cells.append(cell_info)
-    return cells
 
-def output_logfiles_to_csv(log_list, log_csv, srr_gsm_map):
+        if len(cell_info['doc_id']) > 0:
+            doc_id = cell_info['doc_id']
+            sample_list = doc_sample_list[doc_id]
+            if len(cell_info['gsm_id']) > 0 and (cell_info['gsm_id'] not in sample_list):
+                continue # Skip if sample is not included (due to different taxon)
+            doc_to_counts[doc_id] = doc_to_counts.get(doc_id, 0) + 1
+        cells.append(cell_info)
+    return (doc_to_counts, cells)
+
+def output_logfiles_to_csv(log_list, log_csv, doc_sample_list):
     cells = []
     headers = []
     first_file = True
@@ -184,6 +232,10 @@ def output_logfiles_to_csv(log_list, log_csv, srr_gsm_map):
             cell_info['srr_id'] = m.group(2)
             cell_info['taxon'] = m.group(3)
             cell_info['gsm_id'] = SRR_GSM_MAPPING.get(m.group(2)) or ''
+        if len(cell_info['doc_id']) > 0:
+            sample_list = doc_sample_list[cell_info['doc_id']]
+            if len(cell_info['gsm_id']) > 0 and (cell_info['gsm_id'] not in sample_list):
+                continue # Skip if sample is not included (due to different taxon)
         cells.append(cell_info)
     with open(log_csv, 'wb') as csvfile:
         gc_writer = csv.writer(csvfile, delimiter=',')
@@ -212,6 +264,14 @@ def output_logfiles_to_csv(log_list, log_csv, srr_gsm_map):
             row = [headers[idx]]
             for cell in cells:
                 row.append(cell['data'][idx])
+            gc_writer.writerow(row)
+
+def output_metadata_csv(metadata_table, output_file_name, headers):
+    headers = ['doc_id'] + headers
+    with open(output_file_name, 'wb') as csvfile:
+        gc_writer = csv.writer(csvfile, delimiter=',')
+        gc_writer.writerow(headers)
+        for row in metadata_table:
             gc_writer.writerow(row)
 
 def output_htseq_to_csv(gene_cell_table, csv_f, gene_to_idx_table):
@@ -259,13 +319,24 @@ def main():
     results = parser.parse_args()
     if results.s3_path and results.output_csv and results.log_csv and results.taxon and results.metadata_prefix:
         S3_BUCKET=results.s3_path
+        taxon_id = taxon_name_to_id(results.taxon)
         htseq_list =[]
         log_list = []
         doc_list = get_doc_list(S3_BUCKET)
+        metadata_list = []
+        series_summary_list = []
+        doc_to_sample_list = {}
         for doc_id in doc_list:
             htseq_list += get_htseq_files_from_s3(doc_id, results.taxon)
             log_list += get_log_files_from_s3(doc_id, results.taxon)
-            gen_metadata_file(doc_id, results.metadata_prefix)
+            samples_obj = gen_metadata_file(doc_id, results.metadata_prefix)
+            taxon_id = taxon_name_to_id(results.taxon)
+            (doc_samples, samples_metadata) = extract_top_metadata_sample_fields(samples_obj,
+                    taxon_id, doc_id)
+            series_metadata = extract_top_metadata_series_fields(samples_obj, doc_id)
+            doc_to_sample_list[doc_id] = doc_samples
+            series_summary_list.append(series_metadata)
+            metadata_list += samples_metadata
 
         # generating tar ball for all the metadata
         command = "tar cvfz %s.tgz %s" % (results.metadata_prefix, results.metadata_prefix)
@@ -277,18 +348,34 @@ def main():
         print command
         output = subprocess.check_output(command, shell=True)
 
+
+        # output metadata
+
+        if len(metadata_list) > 0:
+            output_file_name = results.metadata_prefix + '.samples.csv'
+            output_metadata_csv(metadata_list, output_file_name, METADATA_TOP_SAMPLE_FIELDS)
+
+
         if len(htseq_list) > 0:
             # start the mapping
-            srr_to_gsm_map = {}
             gene_to_idx_table = get_gene_to_idx_mapping(htseq_list[0])
-            gene_cell_table = generate_gc_table(htseq_list, srr_to_gsm_map, gene_to_idx_table)
+            (doc_to_counts, gene_cell_table) = generate_gc_table(htseq_list, gene_to_idx_table,
+                    doc_to_sample_list)
             output_htseq_to_csv(gene_cell_table, results.output_csv, gene_to_idx_table)
+            if len(series_summary_list) > 0:
+                output_series_list = []
+                for row in series_summary_list:
+                    doc_id = row[0]
+                    if doc_to_counts.get(doc_id, 0) > 0:
+                        output_series_list.append(row)
+                output_file_name = results.metadata_prefix + '.series.csv'
+                output_metadata_csv(output_series_list, output_file_name, METADATA_TOP_SERIES_FIELDS)
 
         else:
             print "No htseq files for %s " % (S3_BUCKET)
 
         if len(log_list) > 0:
-            output_logfiles_to_csv(log_list, results.log_csv, srr_to_gsm_map)
+            output_logfiles_to_csv(log_list, results.log_csv, doc_to_sample_list)
         else:
             print "No log files for %s" % (S3_BUCKET)
 
