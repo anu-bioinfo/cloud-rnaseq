7d4a0de26038fa5206d398d94f28164659bd63d8
James Webber <james.webber@czbiohub.org>
2017-09-28 13:36:22 -0700

    Added log file and log messages for everything that was being printed. Could still add more logs..

--- batch/sra_download/run_star_and_htseq.py
+++ batch/sra_download/run_star_and_htseq.py
@@ -15,6 +15,8 @@ import re
 import threading
 import multiprocessing
 
+import logging
+
 ROOT_DIR = '/mnt'
 DEST_DIR = ROOT_DIR + "/data/hca/"
 S3_BUCKET = 's3://czi-hca/data'
@@ -50,27 +52,35 @@ HTSEQ_THREADS_MAX = 4
 CURR_MIN_VER = 20170301
 
 
-def run_sample(sample_name, doc_id, force_download = False):
-    ''' Example:
+def maybe_log(msg, logger=None, **args):
+    if logger is not None:
+        logger.info(msg, **args)
+    print msg
+
+
+def run_sample(sample_name, doc_id, force_download=False, logger=None):
+    """Example:
        run_sample(SRR1974579, 200067835)
-    '''
+    """
 
 
     # Check if star has been run
     if not force_download:
         s3_dest = S3_BUCKET + '/' + doc_id + '/results/'
         command = "aws s3 ls %s | grep %s.%s.htseq" % (s3_dest, sample_name, TAXON)
-        print command
+        maybe_log(command, logger)
         try:
             output = subprocess.check_output(command, shell=True)
+            maybe_log(output, logger)
             if len(output) > 10:
                 # sample has been downloaded before
                 dateint = int(output[0:10].replace('-', ''))
                 if dateint > CURR_MIN_VER:
+                    maybe_log("%s exists, skipping" % sample_name)
                     return
         except Exception:
             # No big deal. S3 error
-            print "%s doesn't exist yet" % sample_name
+            maybe_log("%s doesn't exist yet" % sample_name, logger)
 
     dest_dir = DEST_DIR + sample_name
     subprocess.check_output("mkdir -p %s" % dest_dir, shell=True)
@@ -79,53 +89,60 @@ def run_sample(sample_name, doc_id, force_download = False):
 
     # copy fast.gz from s3 to local
     s3_source = S3_BUCKET + '/' + doc_id + '/rawdata/' + sample_name + '/'
-    command = "aws s3 cp %s %s/rawdata/ --recursive --recursive --exclude '*' --include '*.fastq.gz'" % (s3_source, dest_dir)
-    print command
+    command = "aws s3 cp %s %s/rawdata/ --recursive --exclude '*' --include '*.fastq.gz'" % (s3_source, dest_dir)
+    maybe_log(command, logger)
     output = subprocess.check_output(command, shell=True)
+    maybe_log(output, logger)
 
     # start running STAR
     # getting input files first
     command = "ls %s/rawdata/*.fastq.gz" % dest_dir
-    print command
+    maybe_log(command, logger)
     reads = subprocess.check_output(command, shell=True).replace("\n", " ")
     if len(reads) < 20:
-        print "Empty reads for %s" % s3_source
+        maybe_log("Empty reads for %s" % s3_source)
         return
+
     command = "cd %s/results; mkdir -p Pass1; cd Pass1;" % dest_dir
     #command += STAR + ' ' + COMMON_PARS + ' --genomeDir ' + GENOME_DIR + ' --sjdbGTFfile ' + SJDB_GTF + ' --readFilesIn ' + reads
     command += STAR + ' ' + COMMON_PARS + ' --runThreadN ' + str(multiprocessing.cpu_count()) +  ' --genomeDir ' + GENOME_DIR + ' --readFilesIn ' + reads
-    print command
+    maybe_log(command, logger)
     sys.stdout.flush()
     output = subprocess.check_output(command, shell=True)
+    maybe_log(output, logger)
+
     # running sam tools
     command = "cd %s/results; %s sort -m 6000000000 -o ./Pass1/Aligned.out.sorted.bam ./Pass1/Aligned.out.bam" % (dest_dir, SAMTOOLS)
-    print command
+    maybe_log(command, logger)
     output = subprocess.check_output(command, shell=True)
-    print output
+    maybe_log(output, logger)
     sys.stdout.flush()
 
     # running samtools index -b
     command = "cd %s/results/Pass1; %s index -b Aligned.out.sorted.bam " % (dest_dir, SAMTOOLS)
-    print command
+    maybe_log(command, logger)
     output = subprocess.check_output(command, shell=True)
-    print output
+    maybe_log(output, logger)
     sys.stdout.flush()
 
     # remove unsorted bam files
-    command = "cd %s/results/Pass1; rm -rf Aligned.out.bam " % (dest_dir)
-    print command
+    command = "cd %s/results/Pass1; rm -rf Aligned.out.bam " % dest_dir
+    maybe_log(command, logger)
     output = subprocess.check_output(command, shell=True)
-    print output
+    maybe_log(output, logger)
+
 
     # remove fastq files
     command = "rm -rf %s/rawdata/*.fastq.gz" % dest_dir
-    print command
+    maybe_log(command, logger)
     output = subprocess.check_output(command, shell=True)
-    print output
+    maybe_log(output, logger)
+
     # generating files for htseq-count
     command = "cd %s/results; %s sort -m 6000000000 -n -o ./Pass1/Aligned.out.sorted-byname.bam  ./Pass1/Aligned.out.sorted.bam " % (dest_dir, SAMTOOLS)
-    print command
+    maybe_log(command, logger)
     output = subprocess.check_output(command, shell=True)
+    maybe_log(output, logger)
 
     sys.stdout.flush()
 
@@ -134,9 +151,10 @@ def run_sample(sample_name, doc_id, force_download = False):
 
 
 class htseqThread(threading.Thread):
-    def __init__(self, htseqParams):
+    def __init__(self, htseqParams, logger=None):
         threading.Thread.__init__(self)
         self.htseqParams = htseqParams
+        self.logger = logger
 
     def run(self):
         doc_id = self.htseqParams['doc_id']
@@ -146,57 +164,62 @@ class htseqThread(threading.Thread):
         # running htseq
 
         command = "cd %s/results; %s -r name -s no -f bam -m intersection-nonempty  ./Pass1/Aligned.out.sorted-byname.bam  %s > htseq-count.txt; rm ./Pass1/Aligned.out.sorted-byname.bam" % (dest_dir, HTSEQ, SJDB_GTF)
-        print command
+        maybe_log(command, self.logger)
         output = subprocess.check_output(command, shell=True)
+        maybe_log(output, self.logger)
 
         # compressed the results dir and move it to s3
         command = "cd %s; tar cvfz %s.%s.tgz results" % (dest_dir, sample_name, TAXON)
-        print command
+        maybe_log(command, self.logger)
         output = subprocess.check_output(command, shell=True)
+        maybe_log(output, self.logger)
 
         # copy htseq and log files out to s3
         s3_dest = S3_BUCKET + '/' + doc_id + '/results/'
         command = "aws s3 cp %s/%s.%s.tgz %s" % (dest_dir, sample_name, TAXON, s3_dest)
-        print command
-        subprocess.check_output(command, shell=True)
+        maybe_log(command, self.logger)
+        output = subprocess.check_output(command, shell=True)
+        maybe_log(output, self.logger)
 
         command = "aws s3 cp %s/results/htseq-count.txt %s%s.%s.htseq-count.txt" % (dest_dir, s3_dest, sample_name, TAXON)
-        print command
-        subprocess.check_output(command, shell=True)
+        maybe_log(command, self.logger)
+        output = subprocess.check_output(command, shell=True)
+        maybe_log(output, self.logger)
 
         command = "aws s3 cp %s/results/Pass1/Log.final.out %s%s.%s.log.final.out" % (dest_dir, s3_dest, sample_name, TAXON)
-        print command
-        subprocess.check_output(command, shell=True)
+        maybe_log(command, self.logger)
+        output = subprocess.check_output(command, shell=True)
+        maybe_log(output, self.logger)
 
         # rm all the files
         command = "rm -rf %s" % dest_dir
-        print command
+        maybe_log(command, self.logger)
         subprocess.check_output(command, shell=True)
 
         sys.stdout.flush()
 
 
-def runHtseq(htseq_jobs):
+def runHtseq(htseq_jobs, logger):
     threads = []
     for htseqParams in htseq_jobs:
-        print "Starting a htseq thread for %s " % htseqParams
-        ht_thread = htseqThread(htseqParams)
+        maybe_log("Starting a htseq thread for %s " % htseqParams, logger)
+        ht_thread = htseqThread(htseqParams, logger)
         ht_thread.start()
         threads.append(ht_thread)
     for t in threads:
         t.join()
 
 
-def run(doc_ids, num_partitions, partition_id):
+def run(doc_ids, num_partitions, partition_id, logger=None):
     htseq_jobs = []
     for doc_id in doc_ids:
         if doc_id.startswith('Undetermined'):
-            print "Skipping file: %s" % doc_id
+            maybe_log("Skipping file: %s" % doc_id, logger)
             continue
 
-        print "Running partition %d of %d for doc %s" % (partition_id, num_partitions, doc_id)
+        maybe_log("Running partition %d of %d for doc %s" % (partition_id, num_partitions, doc_id), logger)
         command = "aws s3 ls %s/%s/rawdata/" % (S3_BUCKET, doc_id)
-        print command
+        maybe_log(command, logger)
         try:
             output = subprocess.check_output(command, shell=True).split("\n")
         except Exception:
@@ -213,25 +236,24 @@ def run(doc_ids, num_partitions, partition_id):
             if idx % num_partitions == partition_id:
                 try:
                     ret = run_sample(sample_name, doc_id)
-                    print "%s : %s " % (doc_id, sample_name)
+                    maybe_log("%s : %s " % (doc_id, sample_name), logger)
                     if ret is not None:
                         htseq_jobs.append(ret)
                     if len(htseq_jobs) >= HTSEQ_THREADS_MAX:
-                        runHtseq(htseq_jobs)
+                        runHtseq(htseq_jobs, logger)
                         htseq_jobs = []
 
                 except subprocess.CalledProcessError, e:
-                    print "Error processing stdout output: %s for sample %s\n" % (e.output, sample_name)
+                    maybe_log("Error processing stdout output: %s for sample %s\n" % (e.output, sample_name), logger)
                 except:
-                    print "Error processing %s. " % sample_name
+                    maybe_log("Error processing %s. " % sample_name, logger, exc_info=True)
 
             idx += 1
 
-    runHtseq(htseq_jobs)
+    runHtseq(htseq_jobs, logger)
 
 
-def main():
-
+def main(logger=None):
     global ROOT_DIR
     global DEST_DIR
 
@@ -265,19 +287,19 @@ def main():
     # download the genome data
 
     command = "mkdir -p %s/genome; aws s3 cp s3://czi-hca/ref-genome/%s %s/genome" % (ROOT_DIR, ref_genome_file, ROOT_DIR)
-    print command
+    maybe_log(command, logger)
     subprocess.check_output(command, shell=True)
 
     command = "cd %s/genome; tar xvfz %s " % (ROOT_DIR, ref_genome_file)
-    print command
+    maybe_log(command, logger)
     subprocess.check_output(command, shell=True)
 
     command = "mkdir -p %s/genome/STAR; aws s3 cp s3://czi-hca/ref-genome/STAR/%s %s/genome/STAR" %  (ROOT_DIR, ref_genome_star_file, ROOT_DIR)
-    print command
+    maybe_log(command, logger)
     subprocess.check_output(command, shell=True)
 
     command = "cd %s/genome/STAR; tar xvfz %s" % (ROOT_DIR, ref_genome_star_file)
-    print command
+    maybe_log(command, logger)
     subprocess.check_output(command, shell=True)
 
 
@@ -285,7 +307,7 @@ def main():
 
     # Load Genome Into Memory
     command = "%s --genomeDir %s --genomeLoad LoadAndExit" % (STAR, GENOME_DIR)
-    print command
+    maybe_log(command, logger)
     subprocess.check_output(command, shell=True)
 
     # run through the samples
@@ -295,13 +317,42 @@ def main():
     num_partitions = int(os.environ['NUM_PARTITIONS'])
     partition_id = int(os.environ['PARTITION_ID'])
     doc_ids = os.environ['EXP_IDS'].split(",")
-    run(doc_ids, num_partitions, partition_id)
+    run(doc_ids, num_partitions, partition_id, logger)
 
     # Remove Genome Into Memory
     command = "%s --genomeDir %s --genomeLoad Remove" % (STAR, GENOME_DIR)
-    print command
+    maybe_log(command, logger)
     subprocess.check_output(command, shell=True)
 
 
 if __name__ == "__main__":
-    main()
+
+    if os.environ.get('AWS_BATCH_JOB_ID'):
+        logging.basicConfig(level=logging.INFO)
+        mainlogger = logging.getLogger(__name__)
+
+        log_file = os.path.join(
+                ROOT_DIR, '{}.log'.format(os.environ['AWS_BATCH_JOB_ID'])
+        )
+        handler = logging.FileHandler(log_file)
+        handler.setLevel(mainlogger.level)
+
+        # create a logging format
+        formatter = logging.Formatter(
+                '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        )
+        handler.setFormatter(formatter)
+
+        # add the handlers to the logger
+        mainlogger.addHandler(handler)
+
+        main(mainlogger)
+
+        command = 'aws s3 cp {} s3://jamestwebber-logs/'.format(log_file)
+        mainlogger.info(command)
+        print command
+
+        handler.close()
+        subprocess.check_output(command, shell=True)
+    else:
+        main()
