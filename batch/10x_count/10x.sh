c3d0b7c5fed85956cec9362f5d7a5b87543fcbd2
James Webber <james.webber@czbiohub.org>
2017-10-03 21:20:32 -0700

    Bit by the 8192-byte limit on aegea, bouncing the script to an S3 location

--- batch/10x_count/10x.sh
+++ batch/10x_count/10x.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if [ $# -lt 4 ]; then
+if [ $# -lt 5 ]; then
   echo "Usage: $0 <S3_INPUT_DIR> <S3_OUTPUT_DIR> <TAXON> <CELL_COUNT>"
   echo "   <S3_INPUT_DIR>: S3 Path for the fastq files"
   echo "   <S3_OUTPUT_DIR>: where the results should go"
   echo "   <TAXON>: mus or homo. mus will use mm10 as reference genome, homo will use hg38 as reference genome"
   echo "   <CELL_COUNT>: approx cell count. eg. 3000"
+  echo "   <S3_SCRIPT_LOC>: script location i.e. s3://jamestwebber-logs/run_10x_count.py"
   exit 1
 fi
 
@@ -13,5 +14,9 @@ S3_INPUT_DIR=$1
 S3_OUTPUT_DIR=$2
 TAXON=$3
 CELL_COUNT=$4
+S3_SCRIPT_LOC=$5
+SCRIPT_NAME=`basename $S3_SCRIPT_LOC`
 
-echo aegea batch submit --execute run_10x_count.py --storage /mnt=2000 --ecr-image sra_download --environment S3_INPUT_DIR=$S3_INPUT_DIR S3_OUTPUT_DIR=$S3_OUTPUT_DIR TAXON=$TAXON CELL_COUNT=$CELL_COUNT --memory-mb 64000 --vcpus 32
+
+COMMAND="aws s3 cp $S3_SCRIPT_LOC .; chmod 755 $SCRIPT_NAME; ./$SCRIPT_NAME"
+echo aegea batch submit --command=\"$COMMAND\" --vcpus 32 --memory 128000 --storage /mnt=2000 --ecr-image sra_download --environment S3_INPUT_DIR=$S3_INPUT_DIR S3_OUTPUT_DIR=$S3_OUTPUT_DIR TAXON=$TAXON CELL_COUNT=$CELL_COUNT
