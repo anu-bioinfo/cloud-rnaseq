8188276f9bb3a5507f8efa1ccedb6e6d6669334e
James Webber <james.webber@czbiohub.org>
2017-10-03 21:12:42 -0700

    Fixed bugs: bad paths in files_to_upload. Also rewrote a bunch of this script to be a bit more PEP8.

--- batch/10x_count/run_10x_count.py
+++ batch/10x_count/run_10x_count.py
@@ -17,111 +17,141 @@ import re
 import threading
 import multiprocessing
 
-#CELL_RANGER = '/cellranger-1.3.1/cellranger'
 CELL_RANGER = 'cellranger'
 GC_TABLE_GENERATOR = 'generate_gc_table_from_cellranger.py'
 
 ROOT_DIR = '/mnt'
-DEST_DIR = ROOT_DIR + "/data/hca/"
 
-# files that should be uploaded outside of the massive tgz
-# path should be relative to the run folder
-files_to_upload = ['outs/raw_gene_bc_matrices_h5.h5',
-                   'outs/genes.tsv',
-                   'outs/barcodes.tsv'
-                   'outs/web_summary.html',
-                   'outs/metrics_summary.csv']
 
 def main():
-
-    global ROOT_DIR
-    global DEST_DIR
+    root_dir = ROOT_DIR
 
     if os.environ.get('AWS_BATCH_JOB_ID'):
-        ROOT_DIR = ROOT_DIR + '/' + os.environ['AWS_BATCH_JOB_ID']
-        DEST_DIR = ROOT_DIR + '/data/hca/'
+        root_dir = os.path.join(root_dir, os.environ['AWS_BATCH_JOB_ID'])
+
+    dest_dir = os.path.join(root_dir, 'data', 'hca')
 
     # required environmental variable
-    TAXON   = os.environ['TAXON']
-    S3_INPUT_DIR  = os.environ['S3_INPUT_DIR'].rstrip('/')
-    S3_OUTPUT_DIR = os.environ['S3_OUTPUT_DIR'].rstrip('/')
-    CELL_COUNT = os.environ['CELL_COUNT']
+    taxon = os.environ.get('TAXON', 'homo')
+    s3_input_dir = os.environ['S3_INPUT_DIR'].rstrip('/')
+    s3_output_dir = os.environ['S3_OUTPUT_DIR'].rstrip('/')
+    cell_count = os.environ['CELL_COUNT']
 
 
+    genome_base_dir = os.path.join(root_dir, "genome/cellranger")
+    if taxon == 'homo':
+        genome_name = 'HG38-PLUS'
+    elif taxon == 'mus':
+        genome_name = 'MM10-PLUS'
+    else:
+        raise ValueError("unknown taxon {}".format(taxon))
 
-    GENOME_BASE_DIR = ROOT_DIR + "/genome/cellranger"
-    GENOME_NAME = 'HG38-PLUS'
-    if TAXON == 'mus':
-        GENOME_NAME = 'MM10-PLUS'
+    # files that should be uploaded outside of the massive tgz
+    # path should be relative to the run folder
+    files_to_upload = [
+        'outs/raw_gene_bc_matrices_h5.h5',
+        'outs/raw_gene_bc_matrices/{}/genes.tsv'.format(genome_name),
+        'outs/raw_gene_bc_matrices/{}/barcodes.tsv'.format(genome_name),
+        'outs/raw_gene_bc_matrices/{}/matrix.mtx'.format(genome_name),
+        'outs/web_summary.html',
+        'outs/metrics_summary.csv'
+    ]
 
-    GENOME_TAR_SORUCE = 's3://czi-hca/ref-genome/cellranger/' + GENOME_NAME + '.tgz'
-    GENOME_DIR = GENOME_BASE_DIR + "/" + GENOME_NAME + "/"
+    genome_tar_source = os.path.join('s3://czi-hca/ref-genome/cellranger/',
+                                     genome_name + '.tgz')
+    genome_dir = os.path.join(genome_base_dir, genome_name)
 
     if os.environ.get('AWS_BATCH_JOB_ID'): # Using BATCH
         # download the ref genome data
-        command = "mkdir -p %s; aws s3 cp %s  %s/" %  (GENOME_BASE_DIR, GENOME_TAR_SORUCE, GENOME_BASE_DIR)
+        os.makedirs(genome_base_dir)
+        command = "aws s3 cp {} {}/".format(genome_tar_source, genome_base_dir)
         print command
         subprocess.check_output(command, shell=True)
 
-        ref_genome_file = os.path.basename(GENOME_TAR_SORUCE)
-        command = "cd %s; tar xvfz %s" % (GENOME_BASE_DIR, ref_genome_file)
+        ref_genome_file = os.path.basename(genome_tar_source)
+        os.chdir(genome_base_dir)
+        command = "tar xvfz {}".format(ref_genome_file)
         print command
         subprocess.check_output(command, shell=True)
 
     sys.stdout.flush()
 
     # download the fastq files
-    sample_id = os.path.basename(S3_INPUT_DIR)
-    result_path = DEST_DIR + sample_id
-    fastq_path = result_path + '/fastqs'
-    output_path = result_path + '/' + sample_id
+    sample_id = os.path.basename(s3_input_dir)
+    result_path = os.path.join(dest_dir, sample_id)
+    fastq_path = os.path.join(result_path, 'fastqs')
 
 
-    command = "mkdir -p %s; aws s3 cp %s %s --recursive" % (fastq_path, S3_INPUT_DIR, fastq_path)
+    os.makedirs(fastq_path)
+    command = "aws s3 cp {} {} --recursive".format(s3_input_dir, fastq_path)
     print command
     subprocess.check_output(command, shell=True)
 
 
     # Run cellranger
-    command = "cd %s; %s count --localmem=40 --nosecondary --cells=%s --sample=%s --id=%s --fastqs=%s --transcriptome=%s" % (result_path, CELL_RANGER, CELL_COUNT, sample_id, sample_id, fastq_path, GENOME_DIR)
+    os.chdir(result_path)
+    command = ("{} count --localmem=40 --nosecondary --cells={}"
+               " --sample={} --id={} --fastqs={} --transcriptome={}").format(
+            CELL_RANGER, cell_count, sample_id,
+            sample_id, fastq_path, genome_dir
+    )
     print command
     try:
-        cmnd_output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True, universal_newlines=True);
+        cmnd_output = subprocess.check_output(command,
+                                              stderr=subprocess.STDOUT,
+                                              shell=True,
+                                              universal_newlines=True)
     except subprocess.CalledProcessError as exc:
         print("Status : FAIL", exc.returncode, exc.output)
-        command = "cd %s; tar cvfz %s.tgz %s" % (result_path, sample_id, sample_id)
+        command = "tar cvfz {}.tgz {}".format(sample_id, sample_id)
         print command
         subprocess.check_output(command, shell=True)
 
-        command = "cd %s; aws s3 cp %s.tgz %s/ " % (result_path, sample_id, S3_OUTPUT_DIR)
+        command = "aws s3 cp {}.tgz {}/".format(os.path.join(result_path,
+                                                             sample_id),
+                                                s3_output_dir)
         print command
         subprocess.check_output(command, shell=True)
         sys.exit(1)
 
     # Move results(websummary, cell-gene table, tarball) data back to S3
     for file_name in files_to_upload:
-        command = "cd {}; aws s3 cp {} {}/".format(
-                result_path, os.path.join(sample_id, file_name), S3_OUTPUT_DIR
+        command = "aws s3 cp {} {}/".format(
+                os.path.join(result_path, sample_id, file_name), s3_output_dir
         )
         print command
         subprocess.check_output(command, shell=True)
 
-    command = "cd %s; tar cvfz %s.tgz %s" % (result_path, sample_id, sample_id)
+
+    command = "tar cvfz {}.tgz {}".format(
+            os.path.join(result_path, sample_id), sample_id
+    )
     print command
     subprocess.check_output(command, shell=True)
 
-    command = "cd %s; aws s3 cp %s.tgz %s/ " % (result_path, sample_id, S3_OUTPUT_DIR)
+
+    command = "aws s3 cp {}.tgz {}/ ".format(
+            os.path.join(result_path, sample_id), s3_output_dir
+    )
     print command
     subprocess.check_output(command, shell=True)
 
-    matrix_dir = "raw_gene_bc_matrices/" + GENOME_NAME
-    command = "cd %s; %s -d %s/outs/%s -f %s.%s.cell-gene.csv -m 500" % (result_path, GC_TABLE_GENERATOR, sample_id, matrix_dir, sample_id, TAXON)
+
+    matrix_dir = "raw_gene_bc_matrices/" + genome_name
+    command = "{} -d {} -f {}.{}.cell-gene.csv -m 500".format(
+            GC_TABLE_GENERATOR, os.path.join(sample_id, 'outs', matrix_dir),
+            sample_id, taxon
+    )
     print command
     subprocess.check_output(command, shell=True)
 
-    command = "cd %s; aws s3 cp %s.%s.cell-gene.csv %s/" % (result_path, sample_id, TAXON, S3_OUTPUT_DIR)
+
+    command = "aws s3 cp {}.{}.cell-gene.csv {}/".format(
+            sample_id, taxon, s3_output_dir
+    )
     print command
     subprocess.check_output(command, shell=True)
+
 
 if __name__ == "__main__":
     main()
